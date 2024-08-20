744a8c95b237a56aee64a0bc383e30d95a7fd9bb
yoyo <a4253607@gmail.com>
2024-08-20 23:42:05 +0800

    新增篩選後的房間資料畫面

---
+++ src/pages/front/Rooms.js
@@ -0,0 +1,12 @@
+import FilterOffcanvas from "../../components/Offcanvas/FilterOffcanvas";
+export default function Rooms() {
+    return (
+        <>
+            <FilterOffcanvas />
+            <section className="container py-5">
+                篩選後的房間資料畫面
+            </section>
+        </>
+
+    )
+}
