748bd0456c9930179b15eed3895be5d58fa45562
yoyo <a4253607@gmail.com>
2024-08-20 23:41:33 +0800

    新增文章列表

---
+++ src/pages/front/Articles.js
@@ -0,0 +1,9 @@
+import PageTitle from "../../components/PageTitle/PageTitle"
+
+export default function Articles(){
+    return(
+        <>
+            <PageTitle title={'文章區'} />
+        </>
+    )
+}
