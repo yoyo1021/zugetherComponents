81319784fa2e654d68d0e5507d8de4c5c7d9abd8
Joe-1021 <a4253607@gmail.com>
2024-08-21 16:30:42 +0800

    add roomData

--- src/pages/front/Rooms.js
+++ src/pages/front/Rooms.js
@@ -2,17 +2,55 @@ import FilterOffcanvas from "../../components/Offcanvas/FilterOffcanvas";
 import { HeartToggle } from "../../components/Input/Input";
 import Alert from "../../components/Alert/Alert";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Rooms() {
-    const [isLogin,setIsLogin] = useState(false);
-    const [showAlert,setShowAlert] = useState(false)
-    function heartToggle(e){
-        if(!isLogin){
+    const [isLogin, setIsLogin] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const roomData = [
+        {
+            roomId: '001',
+            roomTitle: '台北信義',
+            roomStyle: '雙人雅房',
+            floor: '2樓',
+            bed: '上下舖',
+            rent: 18000,
+            size: 10,
+            findPerson: 1,
+            address:'台北市信義區市府路45號'
+        },
+        {
+            roomId: '002',
+            roomTitle: '台北萬華公寓',
+            roomStyle: '一人一室套房',
+            floor: '4樓',
+            bed: '單人床',
+            rent: 10000,
+            size: 7,
+            findPerson: 2,
+            address:'台北市萬華區長順街45號'
+        },
+        {
+            roomId: '003',
+            roomTitle: '新北板橋三人雅房',
+            floor: '2樓',
+            bed: '上下舖',
+            rent: 8000,
+            size: 20,
+            findPerson: 1,
+            address:'新北市板橋區文化路一段26號'
+        },
+    ]
+
+    function heartToggle(e) {
+        e.preventDefault();
+        if (!isLogin) {
             setShowAlert(true);
             e.target.checked = false;
-            setTimeout(()=>{
+            setTimeout(() => {
                 setShowAlert(false);
-            },2000)
-        }
+            }, 2000)
+        };
+
     }
     return (
         <>
@@ -20,24 +58,99 @@ export default function Rooms() {
             <Alert alertTxt={'請先登入'} color={'danger'} status={showAlert} />
             <section className="container py-5">
                 <div className="row row-cols-1 row-cols-lg-2">
+                    {roomData.map((room, i) => {
+                        return (
+                            <div className="col roomCard" data-aos='fade-down' key={i}>
+                                <Link to={`/rooms/${room.roomId}`} >
+                                    <div className="card mb-3">
+                                        <div className="row g-0">
+                                            <div className="col-md-4">
+                                                <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                                    className="img-fluid rounded-start object-cover" alt="..." />
+                                            </div>
+                                            <div className="col-md-8">
+                                                <div className="card-body">
+                                                    <div className="d-flex justify-content-between align-items-center mb-2">
+                                                        <h5 className="card-title">{room.roomTitle}</h5>
+                                                        <div onClick={(e) => e.stopPropagation()}>
+                                                            <HeartToggle id={room.roomId} onChange={heartToggle} />
+                                                        </div>
+                                                        
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2">
+                                                            floor
+                                                        </span>
+                                                        {room.floor}
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2">
+                                                            night_shelter
+                                                        </span>
+                                                        {room.roomStyle}
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2">
+                                                            bed
+                                                        </span>
+                                                        {room.bed}
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2">
+                                                            paid
+                                                        </span>
+                                                        {room.rent}/月(不含水電及管理費月)
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2" style={{ transform: 'rotate(-45deg)' }}>
+                                                            width
+                                                        </span>
+                                                        {room.size}坪
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2">
+                                                            person
+                                                        </span>
+                                                        尋求{room.findPerson}位
+                                                    </div>
+                                                    <div className="mb-2 d-flex align-items-center">
+                                                        <span className="material-symbols-outlined me-2">
+                                                            location_on
+                                                        </span>
+                                                        {room.address}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })}
                     <div className="col roomCard" data-aos='fade-down'>
                         <div className="card mb-3">
                             <div className="row g-0">
                                 <div className="col-md-4">
-                                    <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                                    className="img-fluid rounded-start object-cover" alt="..." />
+                                    <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                        className="img-fluid rounded-start object-cover" alt="..." />
                                 </div>
                                 <div className="col-md-8">
                                     <div className="card-body">
                                         <div className="d-flex justify-content-between align-items-center mb-2">
                                             <h5 className="card-title">Card title</h5>
-                                            <HeartToggle id={'room1'} onChange={heartToggle}/>
+                                            <HeartToggle id={'room1'} onChange={heartToggle} />
                                         </div>
                                         <div className="mb-2 d-flex align-items-center">
                                             <span className="material-symbols-outlined me-2">
                                                 floor
                                             </span>
                                             2樓
+                                        </div>
+                                        <div className="mb-2 d-flex align-items-center">
+                                            <span className="material-symbols-outlined me-2">
+                                                night_shelter
+                                            </span>
+                                            雙人套房
                                         </div>
                                         <div className="mb-2 d-flex align-items-center">
                                             <span className="material-symbols-outlined me-2">
@@ -72,7 +185,7 @@ export default function Rooms() {
                         <div className="card mb-3">
                             <div className="row g-0">
                                 <div className="col-md-4">
-                                    <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                                    <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                         className="img-fluid rounded-start object-cover" alt="..." />
                                 </div>
                                 <div className="col-md-8">
@@ -120,14 +233,14 @@ export default function Rooms() {
                         <div className="card mb-3">
                             <div className="row g-0">
                                 <div className="col-md-4">
-                                    <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                                    <img src="https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                         className="img-fluid rounded-start object-cover" alt="..." />
                                 </div>
                                 <div className="col-md-8">
                                     <div className="card-body">
                                         <div className="d-flex justify-content-between align-items-center mb-2">
                                             <h5 className="card-title">Card title</h5>
-                                            <HeartToggle id={'room3'}/>
+                                            <HeartToggle id={'room3'} />
                                         </div>
                                         <div className="mb-2 d-flex align-items-center">
                                             <span className="material-symbols-outlined me-2">
