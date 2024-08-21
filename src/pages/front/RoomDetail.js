fbe8b8020a497bdbe3c3e6f052fc7c39b7a29ccd
Joe-1021 <a4253607@gmail.com>
2024-08-21 13:37:34 +0800

    add roomDetail page

---
+++ src/pages/front/RoomDetail.js
@@ -0,0 +1,36 @@
+export default function RoomDetail() {
+    return (
+        <>
+            <span className="material-symbols-outlined">
+                bed
+            </span>
+            <span className="material-symbols-outlined">
+                paid
+            </span>
+            <span className="material-symbols-outlined" style={{ transform: 'rotate(-45deg)' }}>
+                width
+            </span>
+            <span className="material-symbols-outlined">
+                person
+            </span>
+            <span className="material-symbols-outlined">
+                kitchen
+            </span>
+            <span className="material-symbols-outlined">
+                local_laundry_service
+            </span>
+            <span className="material-symbols-outlined">
+                fire_extinguisher
+            </span>
+            <span className="material-symbols-outlined">
+                wifi
+            </span>
+            <span className="material-symbols-outlined">
+                assistant_device
+            </span>
+            <span className="material-symbols-outlined">
+                shower
+            </span>
+        </>
+    )
+}
