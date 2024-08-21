b4e95963d18114605a115e32fef6450c956920d9
Joe-1021 <a4253607@gmail.com>
2024-08-21 13:36:56 +0800

    delete useState(unuse)

--- src/components/Range/Range.js
+++ src/components/Range/Range.js
@@ -1,9 +1,6 @@
-import { useState } from "react";
-
-
 export default function Range({ id,lableTxt,min, max, step, setRangeVal, rangeVal, register }) {
     return (
-        <div className="py-3">
+        <div className="py-3 mb-3">
             <div className="position-relative">
                 <label htmlFor={id} className="form-label" >{lableTxt}</label>
                 <input type="range" className="form-range" id={id} min={min} max={max} step={step} value={rangeVal} 
