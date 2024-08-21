4531052650b171899efc553060cb64fb2fb3a2b2
Joe-1021 <a4253607@gmail.com>
2024-08-21 13:35:42 +0800

    修改banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -36,7 +36,6 @@ export default function Banner({ datas }) {
                                 <>
                                     <div className="bannerTxt">
                                         {data.text}
-                                        
                                     </div>
                                 </>
 
