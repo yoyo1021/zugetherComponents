dd426afb87eb9452030806cbf928e79962182d66
Joe-1021 <a4253607@gmail.com>
2024-08-22 18:15:38 +0800

    修改tag

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -1,8 +1,8 @@
-export default function PageTitle({title}){
-    return(
-        <section className='container py-5'>
-        <h1 className="text-center fw-bold">{title}</h1>
-        <hr className="w-100 pb-3" style={{borderWidth:'3px', borderColor:'red'}}/>
-        </section>
+export default function PageTitle({ title }) {
+    return (
+        <div className='w-100 pb-3'>
+            <h1 className="text-center fw-bold">{title}</h1>
+            <hr className="w-100 pb-3" style={{ borderWidth: '3px', borderColor: 'red' }} />
+        </div>
     )
 }
