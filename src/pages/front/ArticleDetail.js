83a9934e5293f4c426deb95a404f84ce2f79ddd2
Joe-1021 <a4253607@gmail.com>
2024-08-23 16:40:30 +0800

    文章頁面

--- src/pages/front/ArticleDetail.js
+++ src/pages/front/ArticleDetail.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom"
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
+
 export default function ArticleDetail(){
+    const {id} = useParams()
+
     return(
-        <>
-        article detail
-        </>
+        <section className="articleDetail container py-5">
+            <Breadcrumb preLink={'/articles'} prePage={'articles'} id={id} />
+        </section>
     )
 }
