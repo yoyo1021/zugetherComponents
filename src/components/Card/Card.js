7217408776c065a59dce6f2f47e1b6890db7c3f6
yoyo <a4253607@gmail.com>
2024-08-24 09:00:17 +0800

    修改合租好處數字背景色

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ export function AdvantageCard({ num, icon, title, content }) {
         <>
             <div className="advantageCard h-100">
                 <div className="card position-relative bg-primary border-white p-4 rounded h-100">
-                    <span className="position-absolute translate-middle-y top-0 px-2 bg-dark text-white" style={{ left: '20px' }}><h3>{num}</h3></span>
+                    <span className="position-absolute translate-middle-y top-0 px-2 bg-primary text-white" style={{ left: '20px' }}><h3>{num}</h3></span>
                     <h2 className="text-white mb-4"><i className={`me-3 ${icon}`}></i>{title}</h2>
                     <p className="mb-0 text-white lh-base">{content}</p>
                 </div>
