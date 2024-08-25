39f5e3b4696026075746928ade0a205a30fb5e21
yoyo <a4253607@gmail.com>
2024-08-25 23:40:05 +0800

    修改chatList

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import './chatlist.scss';
 
 export default function ChatList() {
-    const [showMsg, setShowMsg] = useState(false);
+    const [showMsg, setShowMsg] = useState(true);
+    const [chatRoomOpen, setChatRoomOpen] = useState(false);
+
     function show(e) {
         if (e.target.checked) {
             setShowMsg(true)
@@ -10,86 +12,102 @@ export default function ChatList() {
             setShowMsg(false)
         }
     }
-    const msgData = [
+    function showChatRoom(id) {
+        console.log(id)
+        setChatRoomOpen(true)
+    }
+    const chatListData = [
         {
-            msgId: '001',
+            listId: '001',
             userId: '051564',
             userName: 'Allen粉多錢',
             userPhoto: '',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae tenetur accusamus voluptas placeat dolorum recusandae,' +
-                'nostrum exercitationem, harum iste itaque eius. Facilis laboriosam expedita sapiente iste, perferendis doloremque exercitationem?' +
-                'Ab a aliquid dolorem quas perspiciatis repellendus explicabo, non ea. Sit, voluptates odit aut assumenda totam soluta architecto iure culpa officia repudiandae, aliquam deleniti beatae nemo, nisi illum laborum itaque.' +
-                'Qui in dolorum harum dicta ipsa repudiandae illo reprehenderit! Molestiae hic dicta nemo laborum, voluptatibus inventore odit ea necessitatibus repudiandae? In minus id dolor repudiandae illo quos nam modi nostrum?' +
-                'Facere deserunt veniam exercitationem. Nihil eveniet corporis distinctio fugit facere iusto nam facilis neque obcaecati, fugiat tempore earum sapiente ullam itaque incidunt officia? Repellendus soluta perferendis accusantium modi, possimus ratione.' +
-                'Vero officiis eligendi placeat aliquam, quas suscipit repellendus porro odio hic earum quisquam distinctio consequuntur, delectus fuga reiciendis reprehenderit. Consectetur illo id beatae earum maxime ratione ex dignissimos dolorem quia.'
+            // message :
+            // [
+            //     {
+            //         id: '1',
+            //         userName: 'Alice',
+            //         message: 'Hello!',
+            //         isUser: false
+            //     },
+            //     {
+            //         id: '2',
+            //         userName: 'You',
+            //         message: 'Hi there!',
+            //         isUser: true
+            //     },
+            //     {
+            //         id: '3',
+            //         userName: 'Alice',
+            //         message: 'How are you?',
+            //         isUser: false
+            //     },
+            // ]
         },
         {
-            msgId: '002',
+            listId: '002',
             userId: '056264',
             userName: 'Bill',
             userPhoto: '',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae tenetur accusamus voluptas placeat dolorum recusandae,' +
-                'nostrum exercitationem, harum iste itaque eius. Facilis laboriosam expedita sapiente iste, perferendis doloremque exercitationem?' +
-                'Ab a aliquid dolorem quas perspiciatis repellendus explicabo, non ea. Sit, voluptates odit aut assumenda totam soluta architecto iure culpa officia repudiandae, aliquam deleniti beatae nemo, nisi illum laborum itaque.' +
-                'Qui in dolorum harum dicta ipsa repudiandae illo reprehenderit! Molestiae hic dicta nemo laborum, voluptatibus inventore odit ea necessitatibus repudiandae? In minus id dolor repudiandae illo quos nam modi nostrum?' +
-                'Facere deserunt veniam exercitationem. Nihil eveniet corporis distinctio fugit facere iusto nam facilis neque obcaecati, fugiat tempore earum sapiente ullam itaque incidunt officia? Repellendus soluta perferendis accusantium modi, possimus ratione.' +
-                'Vero officiis eligendi placeat aliquam, quas suscipit repellendus porro odio hic earum quisquam distinctio consequuntur, delectus fuga reiciendis reprehenderit. Consectetur illo id beatae earum maxime ratione ex dignissimos dolorem quia.'
         },
         {
-            msgId: '003',
+            listId: '003',
             userId: '056514514',
             userName: 'Cathy',
             userPhoto: '',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae tenetur accusamus voluptas placeat dolorum recusandae,' +
-                'nostrum exercitationem, harum iste itaque eius. Facilis laboriosam expedita sapiente iste, perferendis doloremque exercitationem?' +
-                'Ab a aliquid dolorem quas perspiciatis repellendus explicabo, non ea. Sit, voluptates odit aut assumenda totam soluta architecto iure culpa officia repudiandae, aliquam deleniti beatae nemo, nisi illum laborum itaque.' +
-                'Qui in dolorum harum dicta ipsa repudiandae illo reprehenderit! Molestiae hic dicta nemo laborum, voluptatibus inventore odit ea necessitatibus repudiandae? In minus id dolor repudiandae illo quos nam modi nostrum?' +
-                'Facere deserunt veniam exercitationem. Nihil eveniet corporis distinctio fugit facere iusto nam facilis neque obcaecati, fugiat tempore earum sapiente ullam itaque incidunt officia? Repellendus soluta perferendis accusantium modi, possimus ratione.' +
-                'Vero officiis eligendi placeat aliquam, quas suscipit repellendus porro odio hic earum quisquam distinctio consequuntur, delectus fuga reiciendis reprehenderit. Consectetur illo id beatae earum maxime ratione ex dignissimos dolorem quia.'
         },
         {
-            msgId: '004',
+            listId: '004',
             userId: '056514',
             userName: 'Jimmy',
             userPhoto: '',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae tenetur accusamus voluptas placeat dolorum recusandae,' +
-                'nostrum exercitationem, harum iste itaque eius. Facilis laboriosam expedita sapiente iste, perferendis doloremque exercitationem?' +
-                'Ab a aliquid dolorem quas perspiciatis repellendus explicabo, non ea. Sit, voluptates odit aut assumenda totam soluta architecto iure culpa officia repudiandae, aliquam deleniti beatae nemo, nisi illum laborum itaque.' +
-                'Qui in dolorum harum dicta ipsa repudiandae illo reprehenderit! Molestiae hic dicta nemo laborum, voluptatibus inventore odit ea necessitatibus repudiandae? In minus id dolor repudiandae illo quos nam modi nostrum?' +
-                'Facere deserunt veniam exercitationem. Nihil eveniet corporis distinctio fugit facere iusto nam facilis neque obcaecati, fugiat tempore earum sapiente ullam itaque incidunt officia? Repellendus soluta perferendis accusantium modi, possimus ratione.' +
-                'Vero officiis eligendi placeat aliquam, quas suscipit repellendus porro odio hic earum quisquam distinctio consequuntur, delectus fuga reiciendis reprehenderit. Consectetur illo id beatae earum maxime ratione ex dignissimos dolorem quia.'
         },
         {
-            msgId: '005',
+            listId: '005',
             userId: '514514',
             userName: 'Zoe',
             userPhoto: '',
-            message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quae tenetur accusamus voluptas placeat dolorum recusandae,' +
-                'nostrum exercitationem, harum iste itaque eius. Facilis laboriosam expedita sapiente iste, perferendis doloremque exercitationem?' +
-                'Ab a aliquid dolorem quas perspiciatis repellendus explicabo, non ea. Sit, voluptates odit aut assumenda totam soluta architecto iure culpa officia repudiandae, aliquam deleniti beatae nemo, nisi illum laborum itaque.' +
-                'Qui in dolorum harum dicta ipsa repudiandae illo reprehenderit! Molestiae hic dicta nemo laborum, voluptatibus inventore odit ea necessitatibus repudiandae? In minus id dolor repudiandae illo quos nam modi nostrum?' +
-                'Facere deserunt veniam exercitationem. Nihil eveniet corporis distinctio fugit facere iusto nam facilis neque obcaecati, fugiat tempore earum sapiente ullam itaque incidunt officia? Repellendus soluta perferendis accusantium modi, possimus ratione.' +
-                'Vero officiis eligendi placeat aliquam, quas suscipit repellendus porro odio hic earum quisquam distinctio consequuntur, delectus fuga reiciendis reprehenderit. Consectetur illo id beatae earum maxime ratione ex dignissimos dolorem quia.'
         },
-
     ]
+
+    const messageData = [
+        {
+            msgId: '1',
+            userName: 'Allen粉多錢',
+            message: 'Hello!',
+            isUser: false
+        },
+        {
+            msgId: '2',
+            userName: 'You',
+            message: 'Hi ',
+            isUser: true
+        },
+        {
+            msgId: '3',
+            userName: 'Allen粉多錢',
+            message: '可以請教妳有關於房間的資訊嗎?',
+            isUser: false
+        },
+    ]
+
     return (
         <>
             <input type="checkbox" name="show" id="show" onChange={show} />
             {showMsg ? (<>
                 <ul className='chatList h-50 '>
-                    {msgData.map((msg, i) => {
+                    {chatListData.map((chatList, i) => {
                         return (
-                            <li className='chatItems p-2' key={msg.msgId}>
-                                <button type='button' className='w-100  btn '>
+                            <li className='chatItem p-2' key={chatList.listId} onClick={() => showChatRoom(chatList.chatListId)}>
+                                <button type='button' className='w-100  btn'>
                                     <div className="row d-flex align-items-center">
                                         <div className="col-4">
                                             <img src="https://images.unsplash.com/photo-1508341591423-4347099e1f19?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1hbnxlbnwwfHwwfHx8MA%3D%3D" alt="" className='object-cover rounded-circle userPhoto' />
                                         </div>
                                         <div className="col-8 text-start">
-                                            <h5 className='fw-bold'>{msg.userName}</h5>
+                                            <h5 className='fw-bold'>{chatList.userName}</h5>
                                             <p className='text-truncate'>
-                                                {msg.message}
+                                                {chatList.message}
                                             </p>
                                         </div>
                                     </div>
@@ -98,27 +116,35 @@ export default function ChatList() {
                         )
                     })}
                 </ul>
-                <div className="chatRoom h-50 border rounded shadow-sm">
-                    <div className="chatHeader d-flex justify-content-between align-items-center p-2 border-bottom">
-                        <h5 className="mb-0">Chat Room</h5>
-                        <button type="button" className="btn-close" aria-label="Close"></button>
-                    </div>
-                    <div className="chatMessages overflow-auto p-3" style={{ height: '340px', backgroundColor: '#f8f9fa' }}>
-                        {msgData.map((msg, i) => (
-                            <div key={msg.msgId} className={`messageItem mb-3 d-flex ${msg.isUser ? 'justify-content-end' : 'justify-content-start'}`}>
-                                <div className={`messageBubble p-2 rounded ${msg.isUser ? 'bg-primary text-white' : 'bg-light text-dark'}`} style={{ maxWidth: '75%' }}>
-                                    <p className="mb-0">{msg.message}</p>
-                                </div>
+                {chatRoomOpen && (
+                    <div className="chatRoom h-50 border rounded shadow-sm">
+                        <div className="chatHeader d-flex justify-content-between align-items-center p-2 border-bottom">
+                            <h5 className="mb-0">Chat Room</h5>
+                            <button type="button" className="btn-close" aria-label="Close" onClick={() => setChatRoomOpen(false)}></button>
+                        </div>
+                        <div className="chatMessages overflow-auto p-3" style={{ height: '340px', backgroundColor: '#f8f9fa' }}>
+                            {messageData.map((msg, i) => {
+                                return (
+                                    <div key={msg.id}>
+                                        <div className={`messageItem mb-3 d-flex ${msg.isUser ? 'justify-content-end' : 'justify-content-start'}`}>
+                                            <img src="https://images.unsplash.com/photo-1508341591423-4347099e1f19?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG1hbnxlbnwwfHwwfHx8MA%3D%3D" alt="" className={`object-cover rounded-circle me-2 ${msg.isUser ? 'd-none':''}`} style={{ width: '40px', height: '40px' }} />
+                                            <div className={`messageBubble p-2 rounded ${msg.isUser ? 'bg-primary text-white' : 'bg-light text-dark'}`} style={{ maxWidth: '75%' }}>
+                                                <p className="mb-0">{msg.message}</p>
+                                            </div>
+                                        </div>
+                                        <p>{msg.id}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="inputGroup ">
+                            <div className="input-group ps-2">
+                                <input type="text" className="form-control" placeholder="請輸入訊息...." />
+                                <button className="btn btn-primary" type="button">Send</button>
                             </div>
-                        ))}
-                    </div>
-                    <div className="inputGroup ">
-                        <div className="input-group ps-2">
-                            <input type="text" className="form-control" placeholder="請輸入訊息...." />
-                            <button className="btn btn-primary" type="button">Send</button>
                         </div>
                     </div>
-                </div>
+                )}
 
             </>) : (<>
                 <p className='text-center text-light fs-1 fw-bold'>尚未有聊天訊息</p>
