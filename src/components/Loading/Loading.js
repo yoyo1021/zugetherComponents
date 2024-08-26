b77ef7133387d670e8cd40d303610a4689d662eb
Joe-1021 <a4253607@gmail.com>
2024-08-26 16:09:10 +0800

    修改loading變數

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,10 @@
 import ReactLoading from 'react-loading';
-export default function Loading({ type, color, isLoading }) {
+export default function Loading({ isLoading }) {
     return (
         <>
             {isLoading && (
                 <div className="position-fixed top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center" style={{ backgroundColor: 'rgba(0,0,0,0.5)', zIndex: 1000, backdropFilter: 'blur(3px)' }}>
-                    <ReactLoading type={type} color={color} height={60} width={100} />
+                    <ReactLoading type={'balls'} color={'gold'} height={60} width={100} />
                 </div>
             )}
 
