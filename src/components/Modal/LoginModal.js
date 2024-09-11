abcd121723a8e0ed1174c670e58dc7486c451de0
Joe-1021 <a4253607@gmail.com>
2024-09-11 16:10:55 +0800

    新增記住帳號功能

--- src/components/Modal/LoginModal.js
+++ src/components/Modal/LoginModal.js
@@ -2,29 +2,67 @@ import { Input } from "../Input/Input";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useLogin } from "../../store/dataStore";
+import { useEffect, useRef, useState } from "react";
 export default function LoginModal({ closeModal }) {
-    const {setIsLogin}= useLogin();
+    const { setIsLogin } = useLogin();
+    const [email,setEmail] = useState('');
+    const memberRef = useRef(null);
     const navigate = useNavigate();
+    let isRemember = localStorage.getItem("remember");
     function goToRegister() {
         navigate('/signup');
         closeModal();
     };
-
+    
     const {
         register,
         handleSubmit,
+        resetField,
+        reset,
+        setValue,
         formState: { errors }
     } = useForm({
         mode: 'onTouched'
     });
 
+    useEffect(()=>{
+        if(isRemember){
+            memberRef.current.checked = true
+            // 防止被偵測未填寫
+            setValue("loginEmail",localStorage.getItem("email"))
+            //email帶值到畫面
+            setEmail(localStorage.getItem("email"))
+            //isRemember = true
+        }else{
+            localStorage.clear();
+            isRemember = false
+        }
+    },[setValue])
+
     const onSubmit = (data) => {
-        const { email, password } = data;
         console.log(data);
+        if(isRemember){
+            resetField("loginPassword")
+        }else{
+            reset();
+        }
         setIsLogin(true);
         closeModal();
     }
 
+    const remember=(e)=>{
+        const {checked} = e.target;
+        if(checked){
+            isRemember = true;
+            localStorage.setItem("email",email);
+            localStorage.setItem("remember",checked);
+        }else{
+            isRemember = false;
+            localStorage.clear();
+        }
+    }
+
+    
     return (
         <>
             <div className="modal fade" id='loginModal' data-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -49,6 +87,8 @@ export default function LoginModal({ closeModal }) {
                                             message: 'Email 格式不正確',
                                         }
                                     }}
+                                    onChange={(e)=>setEmail(e.target.value)}
+                                    value={email}
                                 />
                                 <Input
                                     type='password'
@@ -63,8 +103,22 @@ export default function LoginModal({ closeModal }) {
                                             value: 6,
                                             message: '密碼不少於 6 碼'
                                         }
-                                    }}
+                                    }}  
                                 />
+                                <div className="mb-3">
+                                    <div className="form-check">
+                                        <input
+                                            className="form-check-input"
+                                            type="checkbox"
+                                            id="remember"
+                                            onChange={(e)=>remember(e)}
+                                            ref={memberRef}
+                                        />
+                                        <label className="form-check-label" htmlFor="remember" style={{ lineHeight: '1.5' }}>
+                                            記住 Email
+                                        </label>
+                                    </div>
+                                </div>
                                 <div className="flex-column border-top-0">
                                     <button type="submit" className="btn btn-primary w-100 mb-2">登入</button>
                                     <button type="button" className="btn btn-secondary w-100" onClick={goToRegister}>註冊</button>
