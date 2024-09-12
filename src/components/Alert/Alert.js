981dd86ecc59bf89295de2901f5a300abe72042c
yoyo <a4253607@gmail.com>
2024-09-12 15:41:00 +0800

    修改alert

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,12 +1,33 @@
 import './alert.scss';
 
+
 export default function Alert({ alertTxt, color, status }) {
+    const alertStatus = (color) => {
+        switch (color) {
+            case 'success':
+                return (
+                    <>
+                        <i className={`bi bi-check-circle alert-icon animate__animated animate__flip `}></i>
+                        <strong className='animate__animated animate__pulse '>{alertTxt}</strong>
+                    </>
+                );
+            case 'danger':
+                return (
+                    <>
+                        <i className={`bi bi-x-circle alert-icon animate__animated animate__shakeX `}></i>
+                        <strong className='animate__animated animate__flash'>{alertTxt}</strong>
+                    </>
+                )
+            default: return null
+        }
+    }
     return (
-        <div className={`alertWrapper ${status ? 'd-flex':'d-none'}`} >
-            <div className={`alert alert-${color} position-absolute text-center py-4`} role="alert">
-                {alertTxt}
+        <div className={`alertWrapper ${status ? 'd-flex' : 'd-none'} `} >
+            <div className={`alert alert-${color} position-absolute text-center py-4 text-${color}`} role="alert">
+                <div className="d-flex flex-column ">
+                    {alertStatus(color)}
+                </div>
             </div>
         </div>
-
     )
 }
