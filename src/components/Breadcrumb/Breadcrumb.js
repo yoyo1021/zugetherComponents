0ca49a4b5e1bf640085264f9e68fa47486203858
yoyo <a4253607@gmail.com>
2024-09-17 17:04:56 +0800

    修改麵包屑寫法

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './breadcrumb.scss'
-export default function Breadcrumb({preLink,prePage,id}) {
+export default function Breadcrumb() {
+    const navigate = useNavigate();
     return (
         <nav aria-label="breadcrumb">
             <ol className="breadcrumb">
-                <li className="breadcrumb-item"><Link to={preLink}>{prePage}</Link></li>
-                <li className="breadcrumb-item active" aria-current="page">{id}</li>
+                {/* <li className="breadcrumb-item"><Link to={preLink}>{prePage}</Link></li> */}
+                <li className="breadcrumb-item">
+                    <button className="btn btn-primary" onClick={()=>navigate(-1)}>
+                        <i className="bi bi-arrow-left me-2"></i>上一頁
+                    </button>
+                </li>
             </ol>
         </nav>
     )
